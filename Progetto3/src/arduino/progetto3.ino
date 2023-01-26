c2d16e3652390ba89434f771d97037af43f4be1e
giovanni12perini <99191725+giovanni12perini@users.noreply.github.com>
2023-01-26 21:08:53 +0100

    tolto una cartella inutile

---
+++ Progetto3/src/arduino/progetto3.ino
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <SoftwareSerial.h> 
+#include "Proxy.h" //file.h del sonar
+#include "Led.h"
+#include "Autom.h"
+#include "Manual.h"
+#include "Potentiometer.h"
+#include "Timer.h"
+#include "Task.h"
+#include "Scheduler.h"
+
+SoftwareSerial MyBlue(2, 3); // RX | TX
+Servo engine;
+extern bool autoState;
+extern bool manualState;
+extern char man;
+Timer     timer;
+Scheduler sched;
+
+
+void setup() {
+  Serial.begin(9600);
+  MyBlue.begin(9600);
+  
+  engine.attach(SERVO);
+
+  timer.setupPeriod(50);
+
+  sched.init(50);
+
+  Task* autom  = new Autom(LED1,LED2,LEDM,ECHO,TRIG);
+  wait    -> init(50);
+  sched.addTask(autom);
+
+  Task* manual = new Manual(LED1,LED2,LEDM,ECHO,TRIG);
+  starter -> init(50);
+  sched.addTask(manual);
+
+  for(int i = 0; i < CALIBRATION_TIME_SEC; i++){
+    delay(1000);
+  }
+
+  autoState  = true;
+  manualState = false;
+
+  delay(50);
+
+
+}
+
+void loop() {
+  sched.schedule();
+}
