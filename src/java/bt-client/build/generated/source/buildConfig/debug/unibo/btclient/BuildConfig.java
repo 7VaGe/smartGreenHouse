967ce59bea9c7f9368584bed411a779aa9e6bd60
Nicholas Ciarafoni <78082910+7VaGe@users.noreply.github.com>
2023-11-27 14:01:20 +0100

    Commit di prova novembre
    Ho tipo 450 cambiamenti, che devo capire perchè me li segnala in fetching, ora committo poi vanno controllati

---
+++ src/java/bt-client/build/generated/source/buildConfig/debug/unibo/btclient/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package unibo.btclient;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "it.unibo.isi.seeiot.androidbluetoothex";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
